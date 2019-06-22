f1a943bf6a7ad5ebfe81999ee09e9b34aa9fe322
Paweł Merdała <p.merdala@interia.pl>
2019-06-22 20:00:38 +0200

    testy dla komponentu hero-detail.component

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,30 +1,43 @@
 import { Location } from '@angular/common';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs/observable/of';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+import { Spied } from '../types/spied.type';
 
 describe('HeroDetailComponent', () => {
-  let fixture : ComponentFixture<HeroDetailComponent>;
-  let component : HeroDetailComponent;
-  let hero : Hero;
-  let mockHeroService;
-  let mockActivatedRoute;
-  let mockLocation;
+  let fixture: ComponentFixture<HeroDetailComponent>;
+  let component: HeroDetailComponent;
+  let hero: Hero;
+  let thirdHero: Hero;
+  let mockHeroService: Spied<HeroService>;
+  let mockActivatedRoute: object;
+  let mockLocation: Spied<Location>;
 
   beforeEach(async(() => {
     hero = { id: 1, name: 'SuperDude', strength: 50 };
+    thirdHero = { id: 3, name: 'SpiderrDude', strength: 100 };
 
-    mockHeroService = jasmine.createSpyObj('HeroService', ['getHero', 'updateHero']);
-    mockHeroService.getHero.and.returnValue(of(hero));
+    mockHeroService = jasmine.createSpyObj('HeroService', [
+      'getHero',
+      'updateHero'
+    ]);
+    mockHeroService.getHero.and.callFake((id: number) =>
+      id === 3 ? of(thirdHero) : of(hero)
+    );
     mockHeroService.updateHero.and.returnValue(of(null));
     mockLocation = jasmine.createSpyObj('Location', ['back']);
     mockActivatedRoute = {
-      snapshot: { paramMap: { get: () => '3' } },
+      snapshot: { paramMap: { get: () => '3' } }
     };
 
     TestBed.configureTestingModule({
@@ -32,51 +45,84 @@ describe('HeroDetailComponent', () => {
       providers: [
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
         { provide: HeroService, useValue: mockHeroService },
-        { provide: Location, useValue: mockLocation },
+        { provide: Location, useValue: mockLocation }
       ],
-      schemas: [NO_ERRORS_SCHEMA],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
+    component.hero = hero;
+    expect(component.hero).toEqual(hero);
   });
 
-  it('should call getHero on component init', () => {
-    const spy = spyOn(fixture.componentInstance, 'getHero');
+  describe('ngOnInit', () => {
+    it('should call getHero on component init', () => {
+      const spy = spyOn(fixture.componentInstance, 'getHero');
 
-    component.ngOnInit();
+      component.ngOnInit();
 
-    expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('getHero', () => {
+    it('should call heroService.getHero with hero id and set hero property based on response', () => {
+      component.getHero();
+
+      expect(mockHeroService.getHero).toHaveBeenCalledTimes(1);
+      expect(mockHeroService.getHero).toHaveBeenCalledWith(3);
+      expect(component.hero).toEqual(thirdHero);
+    });
   });
 
-  it('should call heroService.getHero with hero id and set hero property based on response', () => {
-    const thirdHero = { id: 3, name: 'SpiderDude', strength: 100 };
-    mockHeroService.getHero.and.returnValue(of(thirdHero));
+  describe('goBack', () => {
+    it('should call location.back', () => {
+      component.goBack();
+      expect(mockLocation.back).toHaveBeenCalledTimes(1);
+    });
+    it('should call location.back with inject', inject([Location], location => {
+      component.goBack();
+      expect(location.back).toHaveBeenCalledTimes(1);
+    }));
+    it('should call location.back with TestBed.get', () => {
+      const locationSpy: Spied<Location> = TestBed.get(Location);
+      component.goBack();
+      expect(locationSpy.back).toHaveBeenCalledTimes(1);
+    });
+    it('should call location.back when user clicks back button', () => {
+      fixture.detectChanges();
 
-    component.getHero();
+      fixture.debugElement
+        .queryAll(By.css('button'))[0]
+        .triggerEventHandler('click', {});
 
-    expect(mockHeroService.getHero).toHaveBeenCalledTimes(1);
-    expect(mockHeroService.getHero).toHaveBeenCalledWith(3);
-    expect(component.hero).toEqual(thirdHero);
+      expect(mockLocation.back).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should call location.back when user clicks back button', () => {
-    fixture.detectChanges();
+  describe('save', () => {
+    it('should call heroService.updateHero and then call goBack', () => {
+      const goBackSpy: jasmine.Spy = spyOn(component, 'goBack');
+      fixture.detectChanges();
 
-    fixture.debugElement.queryAll(By.css('button'))[0].triggerEventHandler('click', {});
+      component.save();
 
-    expect(mockLocation.back).toHaveBeenCalledTimes(1);
-  });
+      expect(mockHeroService.updateHero).toHaveBeenCalledWith(thirdHero);
+      expect(mockHeroService.updateHero).toHaveBeenCalledTimes(1);
+      expect(goBackSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should call heroService.updateHero and then goBack', () => {
+      const goBackSpy = spyOn(fixture.componentInstance, 'goBack');
+      fixture.detectChanges();
 
-  it('should call heroService.updateHero and then goBack', () => {
-    const goBackSpy = spyOn(fixture.componentInstance, 'goBack');
-    fixture.detectChanges();
+      fixture.debugElement
+        .queryAll(By.css('button'))[1]
+        .triggerEventHandler('click', {});
 
-    fixture.debugElement.queryAll(By.css('button'))[1].triggerEventHandler('click', {});
-
-    expect(mockHeroService.updateHero).toHaveBeenCalledWith(hero);
-    expect(goBackSpy).toHaveBeenCalledTimes(1);
+      expect(mockHeroService.updateHero).toHaveBeenCalledWith(thirdHero);
+      expect(goBackSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,34 +8,31 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero : Hero;
+  @Input() hero: Hero;
 
   constructor(
-    private route : ActivatedRoute,
-    private heroService : HeroService,
-    private location : Location,
-  ) {
-  }
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.getHero();
   }
 
-  getHero() : void {
+  getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
   }
 
-  goBack() : void {
+  goBack(): void {
     this.location.back();
   }
 
-  save() : void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
